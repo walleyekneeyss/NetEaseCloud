58e7702fe1e844302476038e4da71473a2183286
YangShuai <yangshuai.yss@bytedance.com>
2024-04-08 19:06:13 +0800

    feat(apps): Eaayless和scssModule的使用

--- Desktop/329resolutely_forget_lisiwen/src/components/app-footer/index.tsx
+++ Desktop/329resolutely_forget_lisiwen/src/components/app-footer/index.tsx
@@ -1,11 +1,14 @@
 import React, { memo } from 'react'; // ReactNode ReactElement
 import type { FC, ReactNode } from 'react';
+// import './index.css';
+// import styles from './index.module.scss';
+import styles from './index.module.css';
 interface IProps {
   children?: React.ReactNode;
 }
 
 const AppFooter: React.FunctionComponent<IProps> = () => {
   // &本来这一块交叉类型了children 现在由自己开发者自己在Iprops中决定是否要children
-  return <div>AppFooters</div>;
+  return <div className={styles.reactless}>AppFooters</div>;
 };
 export default memo(AppFooter);
