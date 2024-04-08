533ba69577100d701c95ee4b5f2d29da76c73510
YangShuai <yangshuai.yss@bytedance.com>
2024-04-08 13:59:37 +0800

    feat(apps): 热门推荐之前

--- Desktop/329resolutely_forget_lisiwen/src/views/discover/c-views/recommend/c-cpns/tap-banner/style.ts
+++ Desktop/329resolutely_forget_lisiwen/src/views/discover/c-views/recommend/c-cpns/tap-banner/style.ts
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
 
 export const BannerWrapper = styled.div`
+  background: url('https://p1.music.126.net/NSGJv9u2kscunFbCiEpgmA==/109951169477325731.jpg?imageView&blur=40x20')
+    center center/6000px;
   .banner {
     width: 982px;
     height: 285px;
@@ -18,6 +20,32 @@ export const BannerLeft = styled.div`
     overflow: hidden;
     height: 285px;
     .image {
+    }
+  }
+
+  .dots {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    margin: 0 auto;
+    display: flex;
+    justify-content: center;
+    > li {
+      margin: 0 2px;
+
+      .item {
+        display: inline-block;
+        width: 20px;
+        height: 20px;
+        background: url(${require('@/assets/img/banner_sprite.png')}) 3px -343px;
+        cursor: pointer;
+
+        &:hover,
+        &.active {
+          background-position: -16px -343px;
+        }
+      }
     }
   }
 `;
