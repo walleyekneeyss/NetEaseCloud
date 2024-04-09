13b8dd8a36145ba5446eb68494c74e458b19a29c
YangShuai <yangshuai.yss@bytedance.com>
2024-04-09 11:05:35 +0800

    reactInitial

--- 329resolutely_forget_lisiwen/src/App.tsx
+++ 329resolutely_forget_lisiwen/src/App.tsx
@@ -1,43 +1,36 @@
-// icon title alia别名 eject craco npm install @craco/craco/alpha -D  npx eslint .   --  npx eslint --init
-import React, { Suspense } from 'react'; //lazy Suspense
-import routes from './router';
-import { Link, useRoutes } from 'react-router-dom';
+import React, { memo } from 'react'; // ReactNode ReactElement
+import logo from './logo.svg';
+import './App.css';
 
-import Download from './views/download';
-
-// ts的拿到函数类型，和函数返回值类型
-import store, { useAppSelector, useAppDispatch, shallowEqualApp } from './store';
-import { changeMessageAction } from './store/modules/counter';
-import AppHeader from './components/app-header';
-import AppFooter from './components/app-footer';
-
-type GetStateFnType = typeof store.getState;
-type IStateFnType = ReturnType<GetStateFnType>;
-
-const App = () => {
-  const { count, message } = useAppSelector(
-    state => ({
-      // {return state.counter.count}
-      count: state.counter.count,
-      message: state.counter.message
-    }),
-    shallowEqualApp
-  );
-
-  const dispatch = useAppDispatch();
-  const handleChangeMessage = () => {
-    // store.dispatch(changeMessageAction('world hello'));
-    dispatch(changeMessageAction('hello world'));
-  };
+function App() {
   return (
     <div className="App">
-      <AppHeader />
-      <Suspense fallback="loading.....................">
-        <div>{useRoutes(routes)}</div>
-      </Suspense>
-      <AppFooter />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <AppFooter />
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Learn React
+        </a>
+      </header>
     </div>
   );
-};
+}
 
+import type { FC, ReactNode } from 'react';
+// import './index.css';
+// import styles from './index.module.scss';
+import styles from './components/app-footer/index.module.less';
+
+interface IProps {
+  children?: React.ReactNode;
+}
+
+const AppFooter = React.memo(() => {
+  // &本来这一块交叉类型了children 现在由自己开发者自己在Iprops中决定是否要children
+  return <div className={styles.reactless}>AppFooters</div>;
+});
+AppFooter.displayName = 'AppFooter';
 export default App;
--- 329resolutely_forget_lisiwen/src/index.tsx
+++ 329resolutely_forget_lisiwen/src/index.tsx
@@ -1,23 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
-import { ThemeProvider } from 'styled-components';
-
-import 'normalize.css';
-import '@/assets/css/index.less';
-import App from '@/App';
-import { Provider } from 'react-redux';
-import store from './store';
-import theme from './assets/theme';
+import './index.css';
+import App from './App';
+// import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-
 root.render(
-  <Provider store={store}>
-    <ThemeProvider theme={theme}>
-      <HashRouter>
-        <App />
-      </HashRouter>
-    </ThemeProvider>
-  </Provider>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
